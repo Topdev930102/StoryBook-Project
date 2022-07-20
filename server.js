e682d5ea2f05093592e9d0ebe1e036081ecc0607
Topdev930102 <topdev930102@outlook.com>
2022-07-20 18:13:04 +0900

    compile again

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require("express");
 const serverStatic = require("serve-static");
 const path = require("path");
 const app = express();
-app.use(serverStatic(path.join(__dirname, "dist")));
+app.use(serverStatic(path.join(__dirname, "frontend")));
 const port = process.env.PORT || 6006;
 app.listen(port);
